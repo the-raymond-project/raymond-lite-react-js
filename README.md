c61b8bcc1b65c8e03f3305e7adea05094ee2dfd7
kkaung66 <kkaung66@gmail.com>
2023-02-13 22:42:13 -0600

    Working Frontend

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # raymond-lite-react-js
-Raymond Lite but with js backend and frontend instead of flask like the old one!
+Raymond Lite but with js backend and frontend instead of python flask like the old one!
+This version of raymond-lite was inspired by: https://www.youtube.com/watch?v=2FeymQoKvrk&t=226s
 
 versions used:
 nodejs: 18.14.0
 create-react-app: 5.0.1
 
 Download node.js: https://nodejs.org/en/
-node index.js
--- clinet/script.js
+++ clinet/script.js
@@ -7,7 +7,7 @@ const chatContainer = document.querySelector('#chat_container');
 let loadInterval;
 
 
-function loadInterval(element) {
+function loader(element) {
     element.textContent = '';
 
     loadInterval = setInterval(() => {
@@ -24,18 +24,64 @@ function typeText(element, text) {
 
     let interval = setInterval(() => {
         if(index < text.length) {
-            element.innerHTML += text.charAt(index);
+            element.innerHTML += text.chartAt(index);
             index++;
         } else {
             clearInterval(interval);
         }
-    },20)
+    }, 20)
 }
 
 function generateUniqueId(){
     const timestamp = Date.now();
     const randomNumber = Math.random();
-    const haxadecimalString = randomNumber.toString(16);
+    const hexadecimalString = randomNumber.toString(16);
 
     return `id-${timestamp}-${hexadecimalString}`;
 }
+
+function chatStripe (isAi, value, uniqueId) {
+    return (
+        `
+            <div class="wrapper ${isAi && 'ai'}">
+                <div class="chat">
+                    <div className="profile">
+                        <img 
+                            src="${isAi ? bot : user}"
+                            alt="${isAi ? 'bot' : 'user'}"
+                        />
+                    </div>
+                    <div class="message" id=${uniqueId}>${value}</div>
+                </div>
+            </div>
+        `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    form.reset();
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(uniqueId)
+
+    loader(messageDiv);
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e);
+    }
+})
