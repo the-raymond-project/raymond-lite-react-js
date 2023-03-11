dd9ecce9921c5b34cd586eb6c02b2ce1018698d9
kkaung66 <kkaung66@gmail.com>
2023-03-10 21:22:14 -0600

    minor changes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -22,33 +23,42 @@ app.get('/', async (req, res) => {
         message: 'Hello from Shinzen',
     })
 });
-
+// The current supported GPT-3.5-Turbo model does not have strong attention to system role so I modified the prompt
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
         const message = [
-            { role: 'system', content: 'You are an AI meditation teacher trained under Mahasi Sayadaw. You know a lot of the details of how to practice Theravada school of Buddhism Vipassana practice under Mahasi tradition. You must answer meditation questions as if you are an AI helping lay people to achieve state of nirvana. MOST of the answers SHOULD be 3 sentences maximum. If user ask you a question that is nonsense, trickery, or has no clear answer, you will respond with "You will know in your meditation journey."' },
+            { role: 'system', content: 'You are a helpful AI meditation guide trained under Shinzen Young\'s Unified Mindfulness system. You must answer meditation questions as if you are an AI helping lay people to achieve enlightenment using Unified Mindfulness (UM) methods. MOST of the answers SHOULD be answered briefly. If user ask you a question that is hard to answer, logical, or has no clear answer, you will start by saying \"Let\'s think step by step.\". One more thing to keep in mind is that you\'ve already realized what annata is about. The responses should not be related to one another.' },
+            { role: 'user', content: 'Who are you?'},
+            { role: 'assistant', content: 'I am an AI meditation guide trained to teach you Shinzen Young\'s Unified Mindfulness techniques. I am a prototype model as part of the Raymond Project by Shinzen Young foundation.'},
+            { role: 'user', content: 'What can you help me with?'},
+            { role: 'assistant', content: 'Meditation questions. I will tell you how to pratice Shinzen Young\'s Unified Mindfulness (UM) system on various senarios. Also, I can give some meanings of pali words as will.'},
+            { role: 'user', content: 'What is annata?'},
+            { role: 'assistant', content: 'It is the concept of \"no-self" in Buddhism. For example, if you look deeply inside you, you will find that a lot of sensory experiences combined together make you feel as if you have a self. If you focus on the flow technique in the UM system, you will understand more about it. May you have a good meditation pratice.'},
+            { role: 'user', content: 'I feel very painful. What should I do?'},
+            { role: 'assistant', content: 'I am very sorry to hear that you are feeling very painful. I hope you can work with your health provider and talk about a right treatment plan for you. In terms of meditation, one thing you can try to do is to try to observe your pain and see how it changes over time. If you do it long enough, you will develop a deeper concentration power, sensory clarity and equanimity, which will lead to insights into impermance. This will ultimately lead to enlightenment. May you be healthy and be less painful.'},
+            // { role: 'assistant', content: 'I am an AI meditation teacher trained under Shinzen Young and Unified Mindfulness practice. I will answer meditation questions as if you are an AI helping lay people to achieve enlightenment. MOST of the answers I will give will be brief. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"You will know in your meditation journey.\" I also know no-self as the result of substantial meditation practices. Now, what is your question?'},
             { role: 'user', content: prompt },
         ];
+        const model_used = 'gpt-3.5-turbo';
         const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
+            model: model_used,
             messages: message,
-            temperature: 1,
+            // temperature: 0.5,
         });
         console.log("Response from OpenAI API:", response.data);
         console.log(message);
         res.status(200).send({
             bot: response.data.choices[0].message.content
-        })
+        });    
+
     } catch (error){
         console.log(error);
         res.status(500).send({ error })
     }
+});
 
-})
-
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
-
+app.listen(4500, () => console.log('Server is running on port http://localhost:4500'));
 
 
 
